58cee3883427181035e35093575f13807f68bec6
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2021-01-25 20:21:32 +0100

    Añado imagine y cambio un par de cositas

--- views/site/index.php
+++ views/site/index.php
@@ -2,10 +2,15 @@
 
 /* @var $this yii\web\View */
 
+use yii\bootstrap4\Html;
+
 $this->title = 'Índice Adesa80vb';
 ?>
 <div class="site-index">
 
-    
+
+    <?= Html::a('Fotos', ['fotos/index'], ['class' => 'btn btn-success']) ?>
+
+    <?= Html::a('Videos', ['videos/index'], ['class' => 'btn btn-success']) ?>
     
 </div>
