56baccf5f36262745de8d8103ecace566403dcb3
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2021-01-26 20:52:39 +0100

    Permito unas opciones avanzadas para subir foto con fecha/equipo

--- views/fotos/create.php
+++ views/fotos/create.php
@@ -11,7 +11,20 @@ use yii\bootstrap4\Html;
 $this->title = 'Subir una Foto';
 $this->params['breadcrumbs'][] = ['label' => 'Fotos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+$js = <<<SCRIPT
+$(botonAvanzado).click(opcionesAvanzadas);
+
+function opcionesAvanzadas(e){
+    e.preventDefault();
+    $(".field-fotos-fecha").show();
+    $(".field-fotos-equipo_id").show();
+    $(botonAvanzado).hide();
+}
+SCRIPT;
+$this->registerJs($js);
 ?>
+
 <div class="fotos-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -21,21 +34,37 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $form->field($model, 'titulo')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'archivo')->textInput(['maxlength' => true]) ?>
+
+    <?= Html::a('','javascript:void(0)', [
+        'class' => 'fas fa-plus mb-4',
+        'id' => 'botonAvanzado'
+    ]) ?>
     
-    <?= $form->field($model, 'fecha')->widget(DatePicker::class, [
-        'options' => ['placeholder' => 'Introduzca fecha de la foto'],
-        'size' => 'sm',
-        'pluginOptions' => [
-            'autoclose'=> true,
-            'format' => 'yyyy-mm-dd'
-        ]
+    <?= $form->field($model, 'fecha', [
+            'options' => [
+                'style' => 'display: none',
+                'class' => 'mb-3'
+            ]
+        ])->widget(DatePicker::class, [
+            'options' => [
+                'placeholder' => 'Introduzca fecha de la foto',
+            ],
+            'size' => 'sm',
+            'pluginOptions' => [
+                'autoclose'=> true,
+                'format' => 'dd-mm-yyyy',
+            ]
     ]) ?>
 
-    <?= $form->field($model, 'equipo_id')->widget(Select2::class, [
+    <?= $form->field($model, 'equipo_id', [
+            'options' => [
+                'style' => 'display: none',
+                'class' => 'mb-3'
+            ]
+        ])->widget(Select2::class, [
         'data' => $equiposId,
         'options' => [
-            'placeholder' => 'Selecciona un equipo ...',
-            'class'
+            'placeholder' => 'Selecciona un equipo ...'
         ],
         'pluginOptions' => [
             'allowClear' => true
